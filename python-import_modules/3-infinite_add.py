0f5d197e27bf6580651c6e0469c1015bfe382aa0
Chambersd29 <chambers.git29@gmail.com>
2023-09-21 22:49:03 -0700

    suma todos los decimales de la terminal

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+def name():
+    u = 0
+    arg = len(sys.argv)
+    for i in range(arg):
+        if i == arg:
+            break
+    for k in range(1, i + 1):
+        u += int(sys.argv[k])
+    print(u)
+if __name__ == "__main__":
+    main()
