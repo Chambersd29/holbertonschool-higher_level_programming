143897542ce027e21723d536b78f92c8ac6451ea
Chambersd29 <chambers.git29@gmail.com>
2023-09-24 20:28:25 -0700

    in 2 fix code estructure and 11 delete posicion arg

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,11 +1,6 @@
+#!/usr/bin/python3i
+
 def delete_at(my_list=[], idx=0):
-
-    my_list.remove(idx)
-    return my_list
-
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
+    if idx >= 0 and idx < len(my_list):
+        del my_list[idx]
+    return (my_list)
--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    elif idx > len(my_list):
-        return my_list
-    else:
-        pass
-    if my_list[idx] in my_list:
+    if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
-        return my_list
+    return my_list
