42787a194a6fb485c10ff3a9dcf509d63b0ca315
Chambersd29 <chambers.git29@gmail.com>
2023-09-29 19:02:02 -0700

    indica divisibles

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,18 +1,13 @@
+#!/usr/bin/python3
+
 def divisible_by_2(my_list=[]):
-    neww = []
-    for i in my_list:
-        if i % 2 == 0:
-            i = True
-            neww.append(i)
+    new_list = []
+    for num in my_list:
+        if num % 2 == 0:
+            num = True
+            new_list.append(num)
         else:
-            i = False
-            neww.append(i)
-    return neww
+            num = False
+            new_list.append(num)
 
-
-my_list = [0, 1, 2, 3, 4, 5, 6]
-list_result = divisible_by_2(my_list)
-i = 0
-while i < len(list_result):
-    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-    i += 1
+    return new_list
