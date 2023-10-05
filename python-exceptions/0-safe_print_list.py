de30e2cce066defc48ff3f96b1c23769b97fd018
Chambersd29 <chambers.git29@gmail.com>
2023-10-04 20:12:33 -0700

    0: imprime elementos de my_list y devuelve los elementos que se imprimen

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3}
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        printed_count = 0
+        for i in range(x):
+            print(my_list[i], end='')
+            printed_count += 1
+        print()
+        return printed_count
+    except IndexError:
+        print()
+        return printed_count
