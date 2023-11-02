bfa3c8f0bf221c8980e4aac55f6281802b88a992
Chambersd29 <chambers.git29@gmail.com>
2023-11-01 22:15:28 -0700

    06

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define una vacia clase BaseGeometry"""
+
+
+class BaseGeometry:
+    """Representa BaseGeometry"""
+
+	def area(self):
+		"""No implenetado"""
+		raise Exception("area() is not implemented")
