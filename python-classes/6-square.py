203bed05133fb8534f3c6437c6d505dc0d0b0568
Chambersd29 <chambers.git29@gmail.com>
2023-12-14 22:58:20 -0800

    06 square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,39 +1,97 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""
+Este módulo representa la clase Square
+"""
 
 
 class Square:
-    """Represent a square."""
+    """
+    Esta clase define un cuadrado
+    """
 
-    def __init__(self, size=0):
-        """Initialize a new square.
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Este es el constructor de la clase Square.
+        Inicializa un nuevo objeto Square con el size recibido.
 
         Args:
-            size (int): The size of the new square.
+            size (int o float): La longitud de los lados del square.
+            position (int, int): Las coordenadas de impresión del cuadrado.
         """
         self.size = size
+        self.position = position
+
+    def area(self):
+        """
+        Este método retorna el área de un cuadrado.
+
+        Returns:
+            _size ** 2 (el tamaño al cuadrado)
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Este método imprime un cuadrado en stdout usando #,
+        siempre y cuando __size > 0.
+        """
+
+        if self.__size == 0:
+            print()
+        else:
+            if self.__position[1] > 0:
+                for o in range(self.__position[1]):
+                    print()
+
+            for i in range(self.__size):
+                print(f"{' ' * self.__position[0]}{'#' * self.__size}")
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+        """
+        Esta propiedad devuelve el __size
+
+        Returns:
+            retorna el __size.
+        """
+        return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        """
+        Esta función configura el valor de __size
+        """
+        if type(value) != int:
             raise TypeError("size must be an integer")
-        elif value < 0:
+
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+    @property
+    def position(self):
+        """
+        Esta propiedad devuelve las coordenadas.
 
-    def my_print(self):
-        """Print the square with the # character."""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
-        if self.__size == 0:
-            print("")
+        Returns:
+            retorna los puntos donde se imprimirá
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """
+        Esta función configura el valor de __position
+        """
+        if type(value) != tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        if type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
