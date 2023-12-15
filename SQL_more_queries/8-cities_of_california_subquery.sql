90217b9da5335508052db0e6afceb079c2c8e045
Chambersd29 <chambers.git29@gmail.com>
2023-12-14 22:53:48 -0800

    08

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,7 @@
---Enumera todas las ciudades de California
+-- lists all the cities of California that can be found on the database 'hbtn_0d_usa'
+-- the states table contains only one record where name = California, but id can be different 
+-- results must be sorted in ascending order by cities.id
+-- is not allowed to use JOIN kw
 SELECT id, name FROM cities
 WHERE state_id = (
 	SELECT id FROM states
